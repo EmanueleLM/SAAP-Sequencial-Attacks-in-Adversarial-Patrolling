486fdeef97f72166ba8da73f9379b5e9519cbb5e
EmanueleLM <emanuele.lamalfa@mail.polimi.it>
2017-04-17 10:03:54 +0200

    added version for k>2, minor fixes

--- attackprediction.py
+++ attackprediction.py
@@ -9,22 +9,24 @@ Two versions of the algorithms are presented: AttackPredition2 is the version wh
 AttackPrediction is the version for k>2
 """
 
+import numpy as np
+import copy as cp
 import computecovsets as ccs
 
 # Attack Prediciton for k=2 sequencial attacks
 # G is the graph as set of vertices
 # v is the vertex in G where D is placed at time j
-# t is the target chosen as first target attack by A
+# t is the target chosen as first target under attack by A
 # j is the time at which A launch its last resource on target t
 def AttackPrediction2(G, v, t, j):
-    G_temp = G; #use a temporary version of the graph in order to modify its components
-    vertex = G_temp.getVertex(v);
+    G_temp = cp.deepcopy(G); #use a temporary version of the graph in order to modify its components
+    vertex = G_temp.getVertex(t);
     vertex.setDeadline(vertex.getDeadline()-j);#update first target's deadline
     R_best = list();
     U_best = 0;
     for t1 in G_temp.getTargets():
         if t1 != t:
-            R,U = ccs.computecovsets(G_temp, vertex, [t,t1]);
+            R,U = ccs.computecovsets(G_temp, v, [t,t1]);
             if U > U_best: #consider just the best route and its utility
                 U_best = U;
                 R_best = R;
@@ -35,6 +37,18 @@ def AttackPrediction2(G, v, t, j):
 # v is the vertex in G where D is placed at time j
 # T is the set of targets previosusly under--attack by A at time i<j
 # j is the time at which A launch its last resource on target t
-def AttackPrediction(G, v, t, j):
-    #..to be done
+def AttackPrediction(G, v, T, j):
+    G_temp = cp.deepcopy(G); #use a temporary version of the graph in order to modify its components
+    for t in T: #set the deadline on each target previously under attack equal to its deadline minus the time passed till that phase of the game
+        target = G_temp.getVertex(t);
+        target.setDeadline(target.getDeadline()-j);#update target's deadline
+    R_best = list();
+    U_best = 0;
+    for t1 in G_temp.getTargets():
+        if t1 != t:
+            R,U = ccs.computecovsets(G_temp, v, np.append(T,t1));
+            if U > U_best: #consider just the best route and its utility
+                U_best = U;
+                R_best = R;
+    return [R_best,U_best]#return the best route in terms of utility, and the relative utility
     return;

1857032eb795534c58eca8947cdc75e36835a827
EmanueleLM <emanuele.lamalfa@mail.polimi.it>
2017-04-23 14:24:15 +0200

    set up the k>3 function
    set up the k>3 function
    looks like the function could work in this way
    We use a tridimensional matrix to store in the third layer the targets under attack expired or covered by D
    The number of layer is equal to the cardinality of the power set ($\mathcal {P}$) of all the possible combinations from 1 to k
    over |T| targets on graph G
    
    In this way the matrix is big, still we use just \textbf{one} matrix

--- pathfinder.py
+++ pathfinder.py
@@ -15,6 +15,7 @@ import numpy as np;
 import attackprediction as ap
 import graph as gr
 import computecovsets as ccs
+import itertools
 
 #class that models the content of a cell in the dynamic programming matrix M,
 #used to store the routes/utility associated to a feasible expansion of the pathfinder algorithm
@@ -65,21 +66,30 @@ class RouteExpansion(object):
 #class that manages the routes expansion when the number of attack is more than 2
 #we use two different classes since we want the code to be distinct between case where k=2, and k>2        
 class RouteExpansion3(RouteExpansion):
-    def __init__(self, route_si, route_ij, u_ij, covered_targets):
+    def __init__(self, route_si, route_ij, u_ij, covered_targets, t_under_attack):
         super(RouteExpansion3, self).__init__(route_si, route_ij, u_ij);
         self.covered_targets = covered_targets;
+        self.t_under_attack = t_under_attack;
     def expandRoute(self, route_si, route_ij, u_ij, covered_targets):
         super(RouteExpansion3, self).expandRoute(route_si, route_ij, u_ij);
         self.covered_targets = covered_targets;
     def getCoveredTargets(self):
         return self.covered_targets;
+    def getTaretsUnderAttack(self):
+        return self.t_under_attack;
     def setCoveredTargets(self, covered_targets):
         self.covered_targets = covered_targets;
+    def setTargetsUnderAttack(self, t_under_attack):
+        self.t_under_attack = t_under_attack;
+    #function that returns the set of covered targets using the intersection between targets covered so far
+    # and targets under attack 
+    def calculateCoveredTargets(self):
+        return np.intersect1d(self.covered_targets, self.t_under_attack);
     #function that prints the values of the element RouteExpansion3
     def printRouteExpansion(self):
-        print("Route_si: ", self.route_si, " \nRoute_ij: ", self.route_ij, " \nUtility: ", self.u_ij, "\nCovered Targets: ", self.covered_targets);
+        print("Route_si: ", self.route_si, " \nRoute_ij: ", self.route_ij, " \nUtility: ", self.u_ij, "\nCovered Targets: ", self.covered_targets, "\nTargets Under Attack in this scenario: ", self.t_under_attack);
     def __eq__(self, x):
-        return np.array_equal(np.sort(self.getRoute_si),np.sort(x.route_si)) and np.array_equal(np.sort(self.getRoute_ij),np.sort(x.route_ij)) and np.array_equal(np.sort(self.covered_targets), np.sort(x.covered_targets)); #we suppose that two routes are equivalent if they contains the same elements, in the same order (we don't care about utility)            
+        return np.array_equal(np.sort(self.getRoute_si),np.sort(x.route_si)) and np.array_equal(np.sort(self.getRoute_ij),np.sort(x.route_ij)) and np.array_equal(np.sort(self.covered_targets), np.sort(x.covered_targets) and np.array_equal(np.sort(self.t_under_attack), np.sort(x.t_under_attack))); #we suppose that two routes are equivalent if they contains the same elements, in the same order (we don't care about utility)            
     #function for distinguish between two vertices
     def __ne__(self, x):
         return not(self.__eq__(x));
@@ -126,14 +136,32 @@ def PathFinder2(G, v, t):
  
 #G is the graph on which D and A play the game
 #v is the vertex where D stays when the function is invoked
-#t is the set of targets under attack when the function is invoked
-#t_covered is the set of targets covered by D when the function is invoked
+#t is the target under attack when the function is invoked
 #k is the number of resources left to A
-def PathFinder(G, v, t, t_covered, k):
+def PathFinder(G, v, t, k):
     n = len(G.getVertices());#number of vertices on G, used to size dp matrix M
-    M = np.array([[[RouteExpansion3(None, None, 0, None) for i in range(n)] for j in range(n)] for l in range(n)],dtype=RouteExpansion);
-    M[v][0][v if v in np.array(t) else 0].expandRoute(v, None, G.getVertex(v).getValue() if v in np.array(t) else 0, v if v in np.array(t) else 0);
-    M[v][0][v if v in np.array(t) else 0].printRouteExpansion();
+    P_t = list();
+    for i in range(k): #compute the combination of the parts of the targets under attack 
+        P_t.append([p for p in itertools.combinations(G.getTargets(), i)]);
+    print(P_t);#we need a big console for a 2**k--sized print!
+    M = np.array([[[RouteExpansion3(None, None, 0, None, None) for i in range(n)] for j in range(n)] for l in range(n)],dtype=RouteExpansion);
+    M[v][0][v if v==t else 0].expandRoute3(v, None, G.getVertex(v).getValue() if v in np.array(t) else 0, v if v in np.array(t) else 0, v if v==t else None);
+    M[v][0][v if v==t else 0].printRouteExpansion();
+    for l in range(len(P_t)): #associate to each of the matrix M a set of covered targets over the parts of (|T| k) possible targets with k resources
+        for j in range(n-1):
+            for i in range(n):
+                if M[i][j][l].isNone() or l>=bubble_bubble: #this one is to prevent routes' expansion when all the targets are covered/expired (we don't expand a M[][][] if it represent the last layer(a layer of cardinality k))
+                    continue;
+                #check if the vertex on which D stays is covered and add it to t_covered in M?
+                # ...
+                # ...
+                for k_left in range(0,k):
+                    if k_left == k: #full resources attack, the game is (somehow) solved
+                        r_c_min, u_min = ap.AttackPrediction(to_be_completed_bubble_bubble);
+                        r_c_min = r_c_min[0];#take just the route, not the cost
+                        M[i][j][l].expandRoute3(M[i][j].getRoute_si(), r_c_min, min(u_min, M[i][j].getUtility()), M[i][j][l].calculateCoveredTargets(G));#each cell will contain the route_ij which is the route that from i will cover the last target uder attack, the relative utility and the set of covered targets 
+                    else: #for all the possible combinations of attacks from 0 to k_left, update the routes
+                        pass;     
     """
     for all Tua s.t. len(Tua)=k --> covsets();
     for all Tua s.t. len(Tua)=k-1 --> expand routes; PathFinder(G, v.neighbors(), tUTua, t_covered, 1);
@@ -170,4 +198,4 @@ print("bestroute")
 print(PathFinder2(G, 1, 2)); #when D is on vertex 0, recieves an attack on target 1
                              #we expect to loose -0.5 since the first attack is on vertex 1, but the best 
                              #strategy for A is a sim. attack to two targets at the beginning, and D will never cover both of them
-PathFinder(G, 1, [1,2,3,4], 0, 3);
+PathFinder(G, 1, [1,2,3,4], 3);

720db0fa6208dd38c119906a7a05baa71b5ee9bc
EmanueleLM <emanuele.lamalfa@mail.polimi.it>
2017-04-24 13:54:31 +0200

    func that turns a set of targets into an address
    listToString function, see the code for the details

--- targetdictionary.py
+++ targetdictionary.py
@@ -6,6 +6,7 @@ Created on Sun Apr 23 17:46:13 2017
 """
 
 import itertools
+import numpy as np
 
 #Transofrm a list in the form [indexNumber, IndexNumber, ...] into a dictionary
 # which is the composed by the power set of the elements fo the list, up to a cardinality which is at most equal to the number of elements in the list itself
@@ -13,7 +14,6 @@ import itertools
 #In order to access to an element of the dictionary, for example targets whose indexNumeber is 2,3,5 return the function to a fresh new dictionary
 # and call the dictionary in this way 
 # name_of_the_dict['2 3 5'] (any permutation of the previous must be passed in order)
-# TODO: make it possible to pass to the dictionary every permutation
 # please note that the input string must respect the format '<indexNumber><space><indexNumber>'
 #
 #The function takes as input:
@@ -27,7 +27,6 @@ def listToDictionary(l, k):
     index = 0;    
     for i in range(k+1): #compute the combination of the parts of the elements in l, till cardinality k 
         powerset.append([p for p in itertools.combinations(l, i)]);
-
     for i in powerset:
         for j in i:
             regexp = str(j).replace(',', "");
@@ -36,7 +35,23 @@ def listToDictionary(l, k):
             dic[regexp] = index;
             index += 1;
     return dic;
-    
+
+#Function that turns a set of integer into an address used by the dictionary created with listToDictionary()
+#If the input is [1,4,3,2] it returns the string '1 2 3 4' that can be used as dictionary index
+# for a vocabulary created with the function listToDictionary
+#Takes as input:
+# the list of integers l
+#Returns:
+# the string used to access to the vocabulary
+def listToString(l):
+    address = '';
+    l = np.sort(l);
+    for i in range(len(l)):
+        if i==len(l)-1:
+            address += str(l[i]);
+        else:
+            address += str(l[i])+' '; 
+    return address;
 """
 Little testing to see if the algorithms work as expected
 """  
@@ -45,4 +60,6 @@ powerset_targets = list([1,2,3,4]); #create the list of elements
 dic = listToDictionary(powerset_targets, 3); #turn the list into a dictionary
 print(dic); #print whole the dictionary
 print(dic['']); #access to the empty element in the list, we expect 0 as output
-print(dic['2 3 4']); #access to the element formed by 2,3 and 4
+print(dic['1 2 4']); #access to the element formed by 2,3 and 4
+print(len(dic)); #number of elements in the dictionary
+print(dic[listToString([1,3,2])]);

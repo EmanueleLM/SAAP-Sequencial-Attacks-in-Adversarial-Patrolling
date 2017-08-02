5c42d3a4486ca9291b7bf3f5164e59e809918a5d
EmanueleLM <emanuele.lamalfa@mail.polimi.it>
2017-08-02 17:06:20 +0200

    creation of graphs in xml adn aggretate data

--- solvesaap.py
+++ solvesaap.py
@@ -17,7 +17,7 @@ import graph as gr
 import os.path
 import time
 
-graphs_input_path = "C:\\Users\\Ga\\Desktop\\"; # path to find the graphs' description
+graphs_input_path = "C:\\Users\\Ga\\Desktop\\instances\\test\\"; # path to find the graphs' description
 output_path  = "C:\\Users\\Ga\\Desktop\\"; # path to store the output in pseudo-xml format
 graphs = list(); # list that contains the name of each graph file in txt format
 k = 4; # number of resources we want the solver solves for each instance of the graphs specified in graphs
@@ -34,48 +34,54 @@ aggregate_prefix = ['NAME', 'TOPOLOGY', 'NUM_V', 'NUM_T', 'K', 'EXEC_TIME', 'UTI
 
 
 #==============================================================================
-# function that invokes pathfinder for a given specification of the SAAP game (Graph, initial vertex, initial target)
+# function that invokes pathfinder for a given specification of the SAAP game 
 #     and solves it for the instances of sequencial attacks that go from 1 to k
 #     creates a "dom"-like structure that is used to store all the salient elements of the saap solution
+# takes as input
+#   the file, filepath, where there's the graph specification
+#   the initial vertex v
+#   the initial target under attack
+#   number of resources avilable to A
 # returns a list of the files that contains the results of the various saap instances
 #==============================================================================
-def solveSAAP(filepath):
+def solveSAAP(filepath, v, t, k):
     files = list();
-    G, vertices, v, t, topology = createGraphFromFile(filepath); # returns in G the graph, in v the initial vertex where D stays at the beginning of the game, and the initial target attack t          
-    for i in range(k):
-        start_time = time.time();
-        routes = pf.PathFinder(G, v, t, i); # solve the game for a specific instance with a given number of resources 'k' for the Attacker
-        exec_time = (time.time() - start_time); # calculate execution time (little overhead introduced by returning of the function, still not important since we are facing an exponential problem)        
-        # write all the stuff to a file in a xml pseudo-format
-        g_tags = list();
-        o_tags = list();
-        root = et.Element("ROOT");
-        g_tags.append(et.SubElement(root, graph_tags[0])); # G (graph) is the first child node of ROOT
-        for j in range(1,len(graph_tags)):
-            g_tags.append(et.SubElement(g_tags[0], graph_tags[j])); # every element of the graph is a subelement of the graph itself
-        for j in range(len(other_tags)):
-            o_tags.append(et.SubElement(root, other_tags[j]));
-        # follow the order in graph_tags to see what's the content of each of the following element
-        g_tags[1].text = str(np.array(G.getAdjacencyMatrix()));
-        g_tags[2].text = str(vertices);
-        g_tags[3].text = str(v);        
-        g_tags[4].text = str(t);
-        g_tags[5].text = str(len(vertices));
-        g_tags[6].text = str(len(G.getTargets()));
-        g_tags[7].text = str(G.getDensity());
-        g_tags[8].text = topology;
-        # follow the order in other_tags to see what's the content of each of the following element
-        o_tags[0].text = str(i+1);
-        # fill this section up with the other o_tags
-        # o_tags[1].text =
-        # o_tags[2].text =
-        # o_tags[3].text = 
-        # ...
-        o_tags[6].text = str(exec_time);
-        o_tags[7].text = str(routes);
-        tree = et.ElementTree(root);
-        files.append(output_path+"_topology_"+topology+"_vertices_"+str(len(G.getVertices()))+"_density_"+str(G.getDensity())+"_V0_"+str(v)+"_T0_"+str(t)+"_resources_"+str(i+1));
-        tree.write(files[-1]); # write on file
+    G, vertices, density, topology = createGraphFromFile(filepath);    
+
+    start_time = time.time(); # start measuring the time of execution (we don't care if we have a small overhead since we don't start measuring int in the true function, that's because eery instance will have the same (little) overhead)
+    #routes = pf.PathFinder(G, v, t, k); # solve the game for a specific instance with a given number of resources 'k' for the Attacker
+    exec_time = (time.time() - start_time); # calculate execution time (little overhead introduced by returning of the function, still not important since we are facing an exponential problem)        
+    # write all the stuff to a file in a xml pseudo-format
+    g_tags = list();
+    o_tags = list();
+    root = et.Element("ROOT");
+    g_tags.append(et.SubElement(root, graph_tags[0])); # G (graph) is the first child node of ROOT
+    for j in range(1,len(graph_tags)):
+        g_tags.append(et.SubElement(g_tags[0], graph_tags[j])); # every element of the graph is a subelement of the graph itself
+    for j in range(len(other_tags)):
+        o_tags.append(et.SubElement(root, other_tags[j]));
+    # follow the order in graph_tags to see what's the content of each of the following element
+    g_tags[1].text = str(list(G.getAdjacencyMatrix())); # adjacency matrix
+    g_tags[2].text = str(vertices); # specification of each vertex
+    g_tags[3].text = str(v); # initila vertex
+    g_tags[4].text = str(t); # initial target
+    g_tags[5].text = str(len(vertices)); # number of vertices on the graph
+    g_tags[6].text = str(len(G.getTargets())); # number of targets on the graph
+    g_tags[7].text = str(G.getDensity()); # edge density
+    g_tags[8].text = topology; # topology of the graph
+    # follow the order in other_tags to see what's the content of each of the following element
+    o_tags[0].text = str(k+1); # number of resources
+    # fill this section up with the other o_tags
+    # o_tags[1].text =
+    # o_tags[2].text =
+    # o_tags[3].text = 
+    # ...
+    o_tags[6].text = str(exec_time); # execution time
+    #o_tags[7].text = str(routes); # list of all the routes generated by the saap instance
+    tree = et.ElementTree(root);
+    files.append(output_path+"_topology_"+topology+"_vertices_"+str(len(G.getVertices()))+"_density_"+str(G.getDensity())+"_V0_"+str(v)+"_T0_"+str(t)+"_resources_"+str(k+1));
+    tree.write(files[-1]); # write on file
+    
     return files;
 #==============================================================================
 # function that create a graph G from a file that specifies the adjacency matrix at first
@@ -87,40 +93,31 @@ def solveSAAP(filepath):
 #             , deadline is a natural number (0 for vertices, any other for targets)
 #             , e.g. [0,0,0] --> vertex, [1, 0.5, 10] --> target with 0.5 as value, 10 as deadline
 #             , an example of a 3*3 vertices' specification is [[0,0,0],[1,1,4],[1,0.3,5]]
-#         initial vertex as a number (rememeber that the first vertex in the graph is the vertex 0 and so on..)
-#         initial target as a number that identifies the vertex (rememeber that the first vertex in the graph is the vertex 0 and so on..)
-#         topology of the graph, the possible choices are {'graph', 'line', 'star', 'crique', ..}
+#          edge density defined as density = 2|E|/|V|(|V|-1)    
+##         topology of the graph, the possible choices are {'graph', 'line', 'star', 'crique', ..}
 #     all this stuff must be encoded in a pseudo-xml format (just to be a little more polite and clean)
 #     even if you can find an example of psuedo-xml graph in the repo on github, here's one:
 
 #     <G>
 #         <A>[[1,1,1],[1,1,1],[1,1,1]]</A>
 #         <V>[[1,0.3,3],[0,0,0],[1,0.8,12]]</V>
-#         <V0>2</V0>
-#         <T0>0</T0>
+#         <DENSITY>0.3</DENSITY>
 #         <TOPOLOGY>graph</TOPOLOGY>
 #     </G>
 
 #     the previous example specifies a fully connected graph with 3 vertices, 2 targets (index 0 and 2) and a vertex (index 1)
-#     the initial vertex is vertex whose id is 2, i.e. the third(and last) vertex in G
-#     the initial target under attack is vertex whose index is 0, i.e. the first vertex on G
+#     the density is set to 0.3
 #     the topology of the graph ('graph' if it's not a specific topology, 'crique', 'line', 'start' etc. otherwise)
 # the function returns 
 #       a graph G, 
 #       the vertices that compose the graph (each one specify if it's a vertex or a target, its value and its deadline)
-#       the intial vertex v  
-#       the initial target under attack t that can be used to invoke PathFinder
-#       
+#       the density of the graph
+#       the topology of the graph
 #==============================================================================
 def createGraphFromFile(filepath):
-#    elements_check = ["A", "V", "V0", "T0", "TOPOLOGY"]; # elements to check if all the graph's elements are present in the file 
+#    elements_check = ["A", "V", "DENSITY", "TOPOLOGY"]; # elements to check if all the graph's elements are present in the file 
     tree = et.parse(filepath);
     root = tree.getroot();
-    # check if everything is ok with the specification file, otherwise exit
-#    for check in range(len(elements_check)):
-#        if str(elements_check[check])!=str(root[check].tag):
-#            print("Missing ", elements_check[check] ,"or troubles with order");
-#            exit();
     # create the empty Graph and the adjacency matrix by parsing the file (thanks to eval, even if I should not use it :P)
     adj_matrix = np.array(eval(root[0].text)); 
     vertices = np.array(eval(root[1].text)); 
@@ -133,7 +130,7 @@ def createGraphFromFile(filepath):
     for v in vertices:
         G.setAdjacents(V[n], np.array(adj_matrix[n]));
         n += 1;       
-    return [G, vertices, int(root[2].text), int(root[3].text), root[4].text]; # return the graph, the initial vertex where D stays and the iinitial attack target
+    return [G, vertices, float(root[2].text), root[3].text]; # return the graph, the vertices, the density and the topology
 #==============================================================================
 # function that given a xml result coming from a saap solution, prints on screen all the xml file 
 #     takes as input the filepath of the xml file
@@ -207,18 +204,19 @@ Little testing to see if the algorithms work as expected
 verbose = True; # this variable controls whether the output is printed
 if verbose:
     # extract elements from the graph file
-    
-    [printSaapDOM(i, True) for i in solveSAAP(graphs_input_path + "graph1.txt")];    
-    if not(os.path.isfile(aggregate_filepath + aggregate_output)): # if the file does not exists, create it with the prefix
-        prefix = str();
-        for i in aggregate_prefix:
-            prefix += str(i)+'\t';
-        f = open(aggregate_filepath + aggregate_output, "w"); # create the file with the prefix
-        f.write(prefix + '\n');
-    else:
-        f = open(aggregate_filepath + aggregate_output, "a"); # open in appendix mode
-    # write all the results row by row, using the fromGraphToXmlName function as "feeder" to the fromXml2Aggregate function, plus the number of resources of a given instance
-    for i in range(1,k+1):
-        line = fromXml2Aggregate(graphs_input_path, fromGraphToXmlName(graphs_input_path + "graph1.txt")+"_resources_"+str(i));
-        f.write(str(line)+'\n');        
-    f.close(); # close the file
+    for inputgraph in os.listdir(graphs_input_path):
+        print(inputgraph);
+        [printSaapDOM(i, True) for i in solveSAAP(graphs_input_path+inputgraph,0,0,3)];    
+        if not(os.path.isfile(aggregate_filepath + aggregate_output)): # if the file does not exists, create it with the prefix
+            prefix = str();
+            for i in aggregate_prefix:
+                prefix += str(i)+'\t';
+            f = open(aggregate_filepath + aggregate_output, "w"); # create the file with the prefix
+            f.write(prefix + '\n');
+        else:
+            f = open(aggregate_filepath + aggregate_output, "a"); # open in appendix mode
+        # write all the results row by row, using the fromGraphToXmlName function as "feeder" to the fromXml2Aggregate function, plus the number of resources of a given instance
+        for i in range(1,k+1):
+            line = fromXml2Aggregate("C:\\Users\\Ga\\Desktop\\", fromGraphToXmlName(graphs_input_path+inputgraph)+"_resources_"+str(i));
+            f.write(str(line)+'\n');        
+        f.close(); # close the file

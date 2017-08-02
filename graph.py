fc87a838e9b813782d3beed70a22f7a20ad80d8a
EmanueleLM <emanuele.lamalfa@mail.polimi.it>
2017-08-02 17:04:18 +0200

    random connected graph creation,with given density

--- graph.py
+++ graph.py
@@ -65,6 +65,13 @@ class Vertex(object):
             self.deadline = max(-1, deadline);#we can't put it to max(0,deadline), 'cause if D is on a target and the deadline is 0, the target is protected, even if it has been expired from ages
         else:
             self.deadline = 0;
+#   set the adjacent vertices using a binary vector where each entry is 1 if there's a connection between the vertex and another one
+    def setAdjacents(self, adjacents):
+        new_adjacents = np.array([]);
+        for i in range(len(G.getVertices())):
+            if adjacents[i]==1 and i!=self.getVertexNumber():
+                new_adjacents = np.append(new_adjacents, i);
+        self.adjacents = new_adjacents;
 #   function that diminishes the deadline of the target of a quantity j
     def diminishDeadline(self, j):
         self.deadline -= j;
@@ -144,9 +151,29 @@ class Graph(object):
             if v.is_target:
                 T = np.append(T,v.vertex_number);
         return T.astype(int);
+#==============================================================================
+# function that returns the density of a given graps as average number of arcs per node   
+# the number of edges is drawn from a normal distribution with mean, said edge density and defined as epsilon = 2E/T*(T−1)
+#==============================================================================
+    def getDensity(self):
+        tot = 0;
+        n = np.size(self.getAdjacencyMatrix()[0]);
+        SP, SP_cost = np.array(sp.shortest_path(self.getAdjacencyMatrix(),n,n));
+        for i in range(n):
+            for j in range(n):
+                if SP_cost[i,j]==1 and i!=j:
+                    tot += 1;
+        return (tot)/(n*(n-1));
         
 #==============================================================================
-# function that generates a random adjacency matrix with the following input
+# function that set all the deadlines of the targets uniformely between min_deadline and max_deadline
+#         
+#==============================================================================
+    def setAllDeadlines(self, min_deadline, max_deadline):
+        for v in self.getTargets():
+            self.getVertex(v).setDeadline(np.random.randint(min_deadline, max_deadline));
+#==============================================================================
+# function that generates a random adjacency (0,1) matrix with the following input
 #  n is the size of the matrix
 #  p in the probability that a node is connected to another one (we assume indepent the prob that i is connected to j, and i is connected to w different from j)    
 # it returns
@@ -164,7 +191,6 @@ def generateRandMatrix(n, p):
                 M[j][i] = 1;
         l += 1;
     return M;
-
 #==============================================================================
 # function that creates a graph that is composed by n vertices whose values is between (0,1] 
 # and whose deadline is uniformely distributed between 1 and max_deadline
@@ -176,13 +202,13 @@ def generateRandMatrix(n, p):
 # it returns
 #     the graph generated with the previous carachteristics
 # please note that
-#     the deadlines are uniformely distributed between [1, max_deadline]
+#     the deadlines are uniformely distributed between [min_deadline, max_deadline]
 #==============================================================================
-def generateRandomGraph(M, n, p, max_deadline):
+def generateRandomGraph(M, n, p, min_deadline, max_deadline):
     vertices = np.array([]);
     for i in range(n):
         if np.random.rand() <= p:
-            deadline = np.random.randint(1, max_deadline);
+            deadline = np.random.randint(min_deadline, max_deadline+1);
             value = np.random.rand();
             v = Vertex(1, value, deadline);
         else:
@@ -191,25 +217,112 @@ def generateRandomGraph(M, n, p, max_deadline):
     G = Graph(vertices);
     for i in range(n):
         G.setAdjacents(vertices[i], M[i]);
-    return G;
-        
- 
+    return G;    
+# function that returns the shortest path cost between two nodes        
+def getShortestPath(G, i, j):
+    n = np.size(G.getAdjacencyMatrix()[0]);
+    SP, SP_cost = np.array(sp.shortest_path(G.getAdjacencyMatrix(),n,n));
+    return int(SP_cost.item(i,j));    
+#==============================================================================
+# function that returns the diameter of a graph, as the longest of the shortest paths on G
+# takes as input
+#     the graph G
+# returns
+#     the diameter as an integer
+#==============================================================================
+def getDiameter(G):
+    n = np.size(G.getAdjacencyMatrix()[0]);
+    SP, SP_cost = np.array(sp.shortest_path(G.getAdjacencyMatrix(),n,n));
+    return np.matrix.max(SP_cost).astype(int);   
+#==============================================================================
+# function that transforms a shortest path matrix into an adjacency matrix
+# takes as input
+#     the shortest path matrix sp
+# returns
+#     the adjacency matrix where something is connected (even to itself) if a 1 is present in the respective
+#     cell of the matrix
+#==============================================================================
+def fromSpToAdjacency(spmatrix):
+    n = range(np.shape(spmatrix)[0]);
+    adjacency = np.array([[0 for i in n]for j in n]);
+    for i in n:
+        for j in n:
+            if spmatrix[i,j]==1 or spmatrix[i,j]==0:
+                adjacency[i][j]=1;
+    return adjacency;
+    
+#==============================================================================
+# function that adds connections to a graph since its edge density reaches a value in the Neighbo(u)rhood of the input \epsilon
+# takes as input
+#     the adjacency matrix M ,which has to represent a connected graph, if not so connect it by using the fromSpToAdjacency(sp.connectMatrix(SP_cost)) "chain"
+#     the desired edge density e, rounded to the third decimal and with an exceeding of 0.05
+# returns the adjacency matrix of the graph with some new edges
+# please note the each new edges is added to a random vertex, if it's not fully connected (if so, it selects another vertex)
+# REMEBER THAT ONCE YOU CALL THIS FUNCTION YOU MUST RECOMPUTE THE SHORTEST PATH MATRIX!
+#==============================================================================
+def increaseEdgeDensity(adj, e):
+    n = np.shape(adj)[0];
+    density = sum(sum(adj)); # total number of edges, also the autoedges are considered here
+    density -= n; # remove autoedges
+    density /= n*(n-1);
+    # 1.while density < e: 
+    # 2.take random vertex, add an edge, if possible 
+    # 3.goto 1
+    while abs(density-e)>=0.05 and density<e:
+         # pickup a vertex randomly
+        v = np.random.randint(n);
+        for i in np.random.permutation(n): # if we permute the indices, we connect random disconnected elements!
+            if adj[v][i] == 0 and i!=v:
+                adj[v][i]=adj[i][v]=1;
+                density = sum(sum(adj)); # total number of edges, also the autoedges are considered here
+                density -= n; # remove autoedges
+                density /= n*(n-1);
+                break;
+    return adj;
+            
+    
+    
 """
 Little testing to see if the algorithms work as expected
 """               
-verbose = True; # this variable controls whether the output is printed
+verbose = False; # this variable controls whether the output is printed
 if verbose: 
-    G = generateRandomGraph(generateRandMatrix(40, 0.5), 40, 0.3, 10); # generate the graph
-    print("\n Targets on G are:");
-    print([int(i) for i in G.getTargets()]);
+    M = generateRandMatrix(15, 0.15); # generate a random adjacency matrix with n vertices, and a probability to be connected to each other vertex set to p
+    G = generateRandomGraph(M, np.shape(M)[0], 0.2, 0, 0); # generate the graph with the adjacency matrix M
+    #print("\n Targets on G are:");
+    #print([int(i) for i in G.getTargets()]);
+    print("Adjacency matrix:");
     print(G.getAdjacencyMatrix());
         #obtain the shortest path matrix (through a classical sp algorithm)
     n = np.size(G.getAdjacencyMatrix()[0]);
     SP, SP_cost = np.array(sp.shortest_path(G.getAdjacencyMatrix(),n,n));
     
-    print("\n Shortest path matrix:");
-    print(SP);
+    #print("\n Shortest path matrix:");
+    #print(SP);
     
     print("\n Shortest Path's cost Matrix:");
     print(SP_cost);
+    
+    print("Density of the graph is ", G.getDensity());    
+    
+    connected_adj = fromSpToAdjacency(sp.connectMatrix(SP_cost));    
+    print("Adjacency graph of the connected matrix is\n", connected_adj);
+    for i in range(np.shape(connected_adj)[0]):
+        G.getVertex(i).setAdjacents(connected_adj[i]);
+    
+    increased_adj = increaseEdgeDensity(connected_adj, 0.35);
+    print("Want to add a number of nodes s.t. the density is 0.35");
+    print(increased_adj);
+    for i in range(np.shape(increased_adj)[0]):
+        G.getVertex(i).setAdjacents(increased_adj[i]);
+        
+    diameter = np.matrix.max((sp.shortest_path(G.getAdjacencyMatrix(),n,n))[1]).astype(int); # calculate the diameter in order to set the min/max deadlines on G
+    G.setAllDeadlines(diameter, (2*diameter)+1);       
+       
+    print("Now the density is ", G.getDensity());
+    print("The new adjacency matrix is ", G.getAdjacencyMatrix());
+    
+    SP, SP_cost = np.array(sp.shortest_path(G.getAdjacencyMatrix(),n,n));
+    print(SP_cost);
+    
     

a871a6439cabc042b566e3f5bb81cc1bc056c20f
EmanueleLM <emanuele.lamalfa@mail.polimi.it>
2017-08-02 17:03:31 +0200

    added function that connects disconnected graphs

--- shortestpath.py
+++ shortestpath.py
@@ -25,4 +25,48 @@ def shortest_path(matrix,m,n):
     H=nx.Graph(matrix);
     SP_cost = nx.floyd_warshall_numpy(H);
     SP = nx.shortest_path(H);
-    return [SP,SP_cost];
+    return [SP,SP_cost];   
+#==============================================================================
+# function that connects a matrix which entries are the shortest paths between vertices
+# takes as input 
+#     the shortest paths' matrix M where 
+#         an entry of 0 means that the vertex has an auto_edge to intself
+#         an entry of 1 means that a vertex is directly connected to another one
+#         an entry of more than 1 but less/equal than |V| means that two vertices are connected not directly
+#         an entry of 999 means that the two vertices are not connected
+# returns 
+#     the connected matrix M    
+#==============================================================================
+def connectMatrix(M):
+    n = range(np.shape(M)[0]);
+    inf = np.inf; # by default the value of infinite is np.inf
+    counter = np.random.randint(np.shape(M)[0]); # this counter should be used to distribute the arcs uniformely
+    if 999 in M:
+        inf = 999;
+    elif np.inf in M:
+        inf = np.inf;
+    while inf in M:
+        for i in n:
+            if M[counter,i]== inf:
+                M[counter,i]=M[i,counter]=1;
+                for p in n:
+                    if M[counter,p]!=inf:
+                        for q in n:
+                            if M[p,q]==inf and M[counter,p]!=inf and M[q,i]!=inf:
+                                M[p,q]=M[q,p]=M[counter,i]+M[q,i]+M[counter,p]; 
+                counter +=1;
+                if counter >= np.shape(M)[0]:
+                    counter = 0;   
+                break;
+    return M;
+    
+"""test part"""
+verbose = False;
+if verbose:
+    M = np.matrix([[0,999,999,1,2,999],[999,0,1,999,999,999],[999,1,0,999,999,999],[1,999,999,0,1,999],[2,999,999,1,0,999],[999,999,999,999,999,0]]);
+    print(connectMatrix(M));
+    print(shortest_path(np.array(M),6,6)[1]);
+    M = np.matrix([[0,999,999],[999,0,999],[999,999,0]]);
+    print(connectMatrix(M));
+    print(shortest_path(np.array(M), 3, 3)[1]);
+
